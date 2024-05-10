d14210c64dcc9ea8660f84af8019c3166acf8cc6
LisaSchlueter <lschlueter@lbl.gov>
2024-05-10 09:14:37 -0700

    add setup script

---
+++ SetUpEnv.jl
@@ -0,0 +1,17 @@
+import Pkg  
+Pkg.add("Distributions")
+Pkg.add("DataFrames")
+Pkg.add("Plots")
+Pkg.add("Measurements")
+Pkg.add("Measures")
+Pkg.add("Unitful")
+Pkg.add("PropDicts")
+Pkg.add("TypedTables")
+Pkg.add("Printf")
+Pkg.add("LaTeXStrings")
+
+Pkg.add("HDF5")
+Pkg.add("JLD2")
+Pkg.add("git@github.com:legend-exp/LegendHDF5IO.jl.git")
+Pkg.add("git@github.com:legend-exp/LegendDataManagement.jl.git")
+
